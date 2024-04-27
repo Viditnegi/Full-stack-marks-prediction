5788b10f55d46d5b1c2ccc23b73be5cc0f1f61f4
Vidit <viditsn@gmail.com>
2024-04-27 12:01:05 +0530

    some more changes

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function wil l return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+
+setup(
+name='Abandoned-Object-Detection',
+version='0.0.1',
+author='vidit',
+author_email='viditsn@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
